c10cc741416254858b1b2e11312e5eaf696a9a78
UglyNixon <brokan.as@gmail.com>
2023-11-23 15:45:40 +0200

    checks actions ci

--- test.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-
-const value = {
-    name: 'as',
-    age: 6,
-};
-console.log(value);
-console.log(Object.entries(value))
-console.log('LV19UNLA0050015901525'==='LV19UNLA0050015901525')
