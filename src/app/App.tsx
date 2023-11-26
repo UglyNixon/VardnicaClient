e260a2d38a93b652bcf01e81c014b34f87a167ff
UglyNixon <brokan.as@gmail.com>
2023-11-26 23:08:40 +0200

    add modal, fix themes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,25 @@
-import React, { Suspense } from 'react';
-import 'app/styles/index.scss';
+import React, { Suspense, useEffect, useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
+import { Modal } from 'shared/ui/Modal';
 
 function App() {
     const { theme } = useTheme();
+    const [isOpen, setIsOpen] = useState(false);
+    useEffect(() => {
+        document.body.className = theme;
+    }, [theme]);
     return (
         <div className={classNames('app', {}, [theme])}>
 
             <Suspense fallback="">
                 <Navbar />
+                <Modal isOpen={isOpen} onClose={() => setIsOpen(false)} />
+                {/* eslint-disable-next-line i18next/no-literal-string */}
+                <button type="button" onClick={() => setIsOpen(true)}>toggle</button>
                 <div className="content-page">
                     <Sidebar />
                     <AppRouter />
